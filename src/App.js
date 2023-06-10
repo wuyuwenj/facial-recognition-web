5f5e9a51630c96d0310459f983ffce415b7022d6
Jameswu <wuyuwen0@gmail.com>
2023-06-10 01:27:13 -0700

    fix deploy error

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import OpenCVComponent from './components/OpenCVComponent.tsx';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import WebcamComponent from './components/WebcamComponent.tsx';
 function App() {
   return (
     <Router>
-      <Switch>
+      <Routes>
         <Route exact path="/">
           <OpenCVComponent />
         </Route>
         <Route path="/webcam">
           <WebcamComponent />
         </Route>
-      </Switch>
+      </Routes>
     </Router>
   );
 }
